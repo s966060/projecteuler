f31bce5779ff2ee28ee9ac96252cf8abbb1fa02f
Filip Delbaen <filipdelbaen@msn.com>
2016-08-08 00:59:54 +0200

    the sum of the squares versus the square of the sum

---
+++ problem-006/src/main/java/com/fde/SumOfSquaresVersusSquareOfSum.java
@@ -0,0 +1,27 @@
+package com.fde;
+
+/**
+ * Created by filipdelbaen on 08/08/16.
+ */
+public class SumOfSquaresVersusSquareOfSum {
+    public static void main(String[] args) {
+        int max = 100;
+
+        int sumOfSquares = 0;
+        int squareOfSum = 0;
+
+        for(int abc = 0; abc <= max; ++abc) {
+            sumOfSquares += abc * abc;
+            squareOfSum += abc;
+        }
+
+        squareOfSum *= squareOfSum;
+
+        System.out.println("squareOfSum = " + squareOfSum);
+        System.out.println("sumOfSquares = " + sumOfSquares);
+
+        int difference = squareOfSum - sumOfSquares;
+
+        System.out.println("difference = " + difference);
+    }
+}
