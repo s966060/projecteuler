5c0991b4958ed8fab60b44f67b1dacf15f327f49
Filip Delbaen <filipdelbaen@msn.com>
2016-08-13 23:26:59 +0200

    the first steps

---
+++ problem-009/src/main/java/org/fde/PythagorianTriplet.java
@@ -0,0 +1,10 @@
+package org.fde;
+
+/**
+ * Created by filipdelbaen on 13/08/16.
+ */
+public class PythagorianTriplet {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ problem-009/src/main/java/org/fde/TripletIterator.java
@@ -0,0 +1,22 @@
+package org.fde;
+
+/**
+ * Created by filipdelbaen on 13/08/16.
+ */
+public class TripletIterator {
+    private int x;
+    private int y;
+    private int z;
+
+    TripletIterator() {
+        x = 1;
+        y = 2;
+        z = 3;
+    }
+
+    void next() {
+        int equalsCondition = 1000;
+
+
+    }
+}
