b7a32d7265b31dad0d626490f50f29f39efabcf4
Filip Delbaen <filipdelbaen@msn.com>
2016-09-02 21:11:32 +0200

    the divisors sorted from small to large

--- util/src/main/java/org/fde/util/primes/Divisors.java
+++ util/src/main/java/org/fde/util/primes/Divisors.java
@@ -6,6 +6,7 @@ import org.fde.util.combinations.CombinationList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -37,6 +38,7 @@ public class Divisors {
             result.add(product);
         }
 
+        Collections.sort(result);
         return result;
     }
 }
