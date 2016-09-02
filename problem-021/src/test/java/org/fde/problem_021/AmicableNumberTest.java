2c936f20f530c5ffdae96dc8bacdcca53a76c46e
Filip Delbaen <filipdelbaen@msn.com>
2016-09-02 21:43:31 +0200

    progress

--- problem-021/src/test/java/org/fde/problem_021/AmicableNumberTest.java
+++ problem-021/src/test/java/org/fde/problem_021/AmicableNumberTest.java
@@ -1,10 +1,15 @@
 package org.fde.problem_021;
 
+import org.fde.util.primes.Divisors;
 import org.junit.Test;
 
 public class AmicableNumberTest {
     @Test
     public void sumOfAmicableNumbersLessThan_10000() {
+        Divisors divisors = new Divisors();
 
+        for (int number = 1; number <= 10_000; ++number) {
+            divisors.getProperDivisors(number);
+        }
     }
 }
