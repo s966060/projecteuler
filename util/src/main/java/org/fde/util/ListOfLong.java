d4e9ec6b5cb125bad90af1a0bbb0762047673f80
Filip Delbaen <filipdelbaen@msn.com>
2016-09-02 21:19:20 +0200

    list of long numbers

---
+++ util/src/main/java/org/fde/util/ListOfLong.java
@@ -0,0 +1,39 @@
+package org.fde.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by filip on 2/09/2016.
+ */
+public class ListOfLong {
+    private final List<Long> numbers;
+
+    public ListOfLong() {
+        this.numbers = new ArrayList<>();
+    }
+
+    public void add(Long number) {
+        this.numbers.add(number);
+    }
+
+    public long getSum() {
+        long sum = 0;
+
+        for (Long number : numbers) {
+            sum += number;
+        }
+
+        return sum;
+    }
+
+    public long getProduct() {
+        long product = 0;
+
+        for (Long number : numbers) {
+            product *= number;
+        }
+
+        return product;
+    }
+}
