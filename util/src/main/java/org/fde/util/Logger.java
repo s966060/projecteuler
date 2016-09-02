6d253e7346f60a0f6284b86f5025d70f973f859f
Filip Delbaen <filipdelbaen@msn.com>
2016-09-02 22:06:51 +0200

    the logger

---
+++ util/src/main/java/org/fde/util/Logger.java
@@ -0,0 +1,11 @@
+package org.fde.util;
+
+public class Logger {
+    public static void log(String format, Object... args) {
+        System.out.printf(format, args);
+    }
+
+    public static void log(String msg) {
+        System.out.println(msg);
+    }
+}
