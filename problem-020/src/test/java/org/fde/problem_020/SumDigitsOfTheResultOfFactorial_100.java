95613210b7a0b61cf9ef9a4e0c2bcd085d365e5e
Filip Delbaen <filipdelbaen@msn.com>
2016-09-02 20:26:14 +0200

    the solution

---
+++ problem-020/src/test/java/org/fde/problem_020/SumDigitsOfTheResultOfFactorial_100.java
@@ -0,0 +1,37 @@
+package org.fde.problem_020;
+
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertEquals;
+
+public class SumDigitsOfTheResultOfFactorial_100 {
+    @Test
+    public void sumOfDigits() {
+        BigInteger factorial = factorial(BigInteger.valueOf(100L));
+        System.out.println("factorial = " + factorial);
+
+        String asString = factorial.toString();
+
+        int sum = 0;
+
+        for(char ch : asString.toCharArray()) {
+            sum += (ch - '0');
+        }
+
+        System.out.println("sum = " + sum);
+        assertEquals(648, sum);
+    }
+
+    public static BigInteger factorial(BigInteger n) {
+        BigInteger result = BigInteger.ONE;
+
+        while (!n.equals(BigInteger.ZERO)) {
+            result = result.multiply(n);
+            n = n.subtract(BigInteger.ONE);
+        }
+
+        return result;
+    }
+}
