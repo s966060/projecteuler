05dbd8d6124cee6aa31a8fa958d9667becf94a33
Filip Delbaen <filipdelbaen@msn.com>
2016-09-03 01:03:42 +0200

    the solution

--- problem-021/src/test/java/org/fde/problem_021/AmicableNumberTest.java
+++ problem-021/src/test/java/org/fde/problem_021/AmicableNumberTest.java
@@ -4,21 +4,60 @@ import org.fde.util.ListOfLong;
 import org.fde.util.primes.Divisors;
 import org.junit.Test;
 
-class Target {
-    private final long number;
-    private final long d;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Target(long number, long d) {
+import static org.junit.Assert.assertEquals;
+
+class Target {
+    private final int number;
+    private final int d;
+
+    public Target(int number, int d) {
         this.number = number;
         this.d = d;
     }
 
-    public long getNumber() {
+    public int getNumber() {
         return number;
     }
 
-    public long getD() {
+    public int getD() {
         return d;
+    }
+
+    @Override
+    public String toString() {
+        return "Target{" +
+                "number=" + number +
+                ", d=" + d +
+                '}';
+    }
+}
+
+class Match {
+    private final Target a;
+    private final Target b;
+
+    public Match(Target a, Target b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "Match{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
+
+    public Target getA() {
+        return a;
+    }
+
+    public Target getB() {
+        return b;
     }
 }
 
@@ -27,10 +66,48 @@ public class AmicableNumberTest {
     public void sumOfAmicableNumbersLessThan_10000() {
         Divisors divisors = new Divisors();
 
-        for (int number = 1; number <= 10_000; ++number) {
-            ListOfLong list = divisors.getProperDivisors(number);
+        List<Target> targets = new ArrayList<>();
 
-            list.getSum();
+        for (int number = 0; number <= 10_000; ++number) {
+            ListOfLong list = divisors.getProperDivisors(number);
+            int d = (int) list.getSum();
+
+            targets.add(new Target(number, d));
         }
+
+        List<Match> matches = new ArrayList<>();
+
+        for (int index = 0; index < targets.size(); ++index) {
+            Target a = targets.get(index);
+
+            if (a != null) {
+                if (a.getD() < targets.size()) {
+                    Target b = targets.get(a.getD());
+
+                    if (b != null) {
+                        if (b.getNumber() != a.getNumber()) {
+                            if (b.getD() == a.getNumber()) {
+                                matches.add(new Match(a, b));
+
+                                targets.set(b.getNumber(), null);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("matches = " + matches);
+
+        int sum = 0;
+
+        for (Match match : matches) {
+            sum += match.getA().getNumber();
+            sum += match.getB().getNumber();
+        }
+
+        System.out.println("sum = " + sum);
+
+        assertEquals(31626, sum);
     }
 }
