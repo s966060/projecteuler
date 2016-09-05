b9bdb1ae0eb5716082af8f3b8cdeca137de79e9d
Filip Delbaen <filipdelbaen@msn.com>
2016-09-06 00:35:02 +0200

    the ordered permutation factory

--- util/src/main/java/org/fde/util/permutations/OrderedPermutationFactory.java
+++ util/src/main/java/org/fde/util/permutations/OrderedPermutationFactory.java
@@ -10,6 +10,14 @@ public class OrderedPermutationFactory {
     }
 
     public PermutationList getPermutations() {
+        ListOfLong original = new ListOfLong(input);
+
+        for(int index = 0; index < original.size(); ++index) {
+            ListOfLong next = new ListOfLong(original);
+            Long first = next.get(index);
+            next.removeAt(index);
+            next = new ListOfLong(first, next);
+        }
         return null;
     }
 }
