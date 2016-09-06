fc09ebbb73c14ff0b82bc7c45dfd11fa7be5ebdb
Filip Delbaen <filipdelbaen@msn.com>
2016-09-07 00:10:41 +0200

    the ordered permutation iterator set up

---
+++ util/src/main/java/org/fde/util/permutations/OrderedPermutationIterator.java
@@ -0,0 +1,15 @@
+package org.fde.util.permutations;
+
+import java.util.Iterator;
+
+public class OrderedPermutationIterator implements Iterator<Permutation> {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Permutation next() {
+        return null;
+    }
+}
