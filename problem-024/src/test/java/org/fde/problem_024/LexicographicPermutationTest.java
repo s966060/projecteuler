608fd1add146ad33d9e68d74830eff0f28b1a5d8
Filip Delbaen <filipdelbaen@msn.com>
2016-09-07 00:08:33 +0200

    the solution for problem 24

--- problem-024/src/test/java/org/fde/problem_024/LexicographicPermutationTest.java
+++ problem-024/src/test/java/org/fde/problem_024/LexicographicPermutationTest.java
@@ -1,7 +1,25 @@
 package org.fde.problem_024;
 
-/**
- * Created by filipdelbaen on 05/09/16.
- */
+import org.fde.util.ListOfLong;
+import org.fde.util.permutations.OrderedPermutationFactory;
+import org.fde.util.permutations.Permutation;
+import org.fde.util.permutations.PermutationList;
+import org.junit.Test;
+
+import static org.fde.util.ListOfLong.*;
+import static org.fde.util.permutations.Permutation.*;
+import static org.junit.Assert.assertEquals;
+
 public class LexicographicPermutationTest {
+    @Test
+    public void theValueOfTheOneMillionthPermutation() {
+        ListOfLong input = createListOfLong(0L, 1L, 2, 3, 4, 5, 6, 7, 8, 9);
+        OrderedPermutationFactory factory = new OrderedPermutationFactory(input);
+        PermutationList list = factory.getPermutations();
+
+        Permutation actual = list.get(999_999);
+
+        Permutation expected = createPermutation(2, 7, 8, 3, 9, 1, 5, 4, 6, 0);
+        assertEquals(expected, actual);
+    }
 }
