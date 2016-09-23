6f8283de480c168f5a7a4bd7b5dcea1c702cb6c1
Filip Delbaen <filipdelbaen@msn.com>
2016-09-23 22:25:04 +0200

    clean up

--- problem-028/src/test/java/org/fde/projecteuler/problem_028/NumberSpiralSquareTest.java
+++ problem-028/src/test/java/org/fde/projecteuler/problem_028/NumberSpiralSquareTest.java
@@ -4,9 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by filipdelbaen on 23/09/16.
- */
 public class NumberSpiralSquareTest {
     @Test
     public void sizeOne() {
