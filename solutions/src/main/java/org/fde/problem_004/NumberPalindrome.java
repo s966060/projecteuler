37127976141d374b2b4c540b322015a57974ab59
Filip Delbaen <filipdelbaen@msn.com>
2016-09-24 00:52:41 +0200

    moving

---
+++ solutions/src/main/java/org/fde/problem_004/NumberPalindrome.java
@@ -0,0 +1,37 @@
+package org.fde.problem_004;
+
+import org.fde.util.Logger;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class NumberPalindrome {
+    public static void main(String[] args) {
+        List<Palindrome> list = new ArrayList<>();
+
+        for (int i = 999; i >= 100; --i) {
+            for (int j = 999; j >= 100; --j) {
+                int r = i * j;
+
+                String a = Integer.toString(r);
+                String b = new StringBuilder(a).reverse().toString();
+
+                if (a.equals(b)) {
+                    list.add(new Palindrome(i, j));
+                }
+            }
+        }
+
+        Collections.sort(list, new Comparator<Palindrome>() {
+            @Override
+            public int compare(Palindrome o1, Palindrome o2) {
+                int compare = Integer.compare(o1.getPalindrome(), o2.getPalindrome());
+                return -compare;
+            }
+        });
+
+        Logger.log("list = " + list);
+    }
+}
