bd776b75c6e8f4d96321913c565a0211b70fd08e
Filip Delbaen <filipdelbaen@msn.com>
2016-10-09 23:01:43 +0200

    my long class

---
+++ util/src/main/java/org/fde/util/LongX.java
@@ -0,0 +1,49 @@
+package org.fde.util;
+
+public class LongX implements Comparable<LongX> {
+    private final long value;
+
+    private LongX(long value) {
+        this.value = value;
+    }
+
+    public LongX add(LongX other) {
+        return LongX.valueOf(this.value + other.value);
+    }
+
+    public LongX mul(LongX other) {
+        return LongX.valueOf(this.value * other.value);
+    }
+
+    private static LongX valueOf(long value) {
+        return new LongX(value);
+    }
+
+    @Override
+    public int compareTo(LongX other) {
+        return Long.compare(this.value, other.value);
+    }
+
+    @Override
+    public String toString() {
+        return "LongX{" +
+                "value=" + value +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LongX longX = (LongX) o;
+
+        return value == longX.value;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (value ^ (value >>> 32));
+    }
+}
