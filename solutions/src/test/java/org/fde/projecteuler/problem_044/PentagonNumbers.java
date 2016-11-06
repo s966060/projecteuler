58e83267bd8b1938ab35b85a4ff9be911d179692
Filip Delbaen <filipdelbaen@msn.com>
2016-11-06 22:50:55 +0100

    the solution for problem 44

--- solutions/src/test/java/org/fde/projecteuler/problem_044/PentagonNumbers.java
+++ solutions/src/test/java/org/fde/projecteuler/problem_044/PentagonNumbers.java
@@ -3,23 +3,74 @@ package org.fde.projecteuler.problem_044;
 import org.fde.util.ListOfLong;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PentagonNumbers {
     @Test
     public void pentagonNumbers() {
-        ListOfLong numbers = new ListOfLong();
+        X x = new X();
 
-        long pentagonal = 1;
-        numbers.add(pentagonal);
-        long add = 4;
+        Long D = null;
 
-        boolean done = false;
+        for (int n = 1; D==null; ++n) {
+            if(n % 1000 == 0) {
+                System.out.println("progress ... @ n = " + n);
+            }
 
-        while (!done) {
-            pentagonal += add;
-            numbers.add(pentagonal);
+            long one = x.getByIndex(n);
 
+            for (int k = 1; k < n; ++k) {
+                long two = x.getByIndex(k);
 
-            add += 3;
+                long diff = one - two;
+
+                if(x.isPentagonal(diff)) {
+                    long sum = one + two;
+
+                    if(x.isPentagonal(sum)) {
+                        System.out.println("n = " + n);
+                        System.out.println("k = " + k);
+                        System.out.println("one = " + one);
+                        System.out.println("two = " + two);
+                        System.out.println("diff = " + diff);
+                        System.out.println("sum = " + sum);
+                        D = diff;
+                    }
+                }
+            }
+        }
+
+        assertEquals(5482660L, D.longValue());
+    }
+
+    class X {
+        private final ListOfLong list;
+
+        X() {
+            this.list = new ListOfLong();
+        }
+
+        long getByIndex(int index) {
+            while (index >= this.list.size()) {
+                long pentagonal = compute(this.list.size());
+                this.list.add(pentagonal);
+            }
+
+            return this.list.get(index);
+        }
+
+        long compute(long index) {
+            long pentagonal = index * ((3 * index) - 1) / 2;
+            return pentagonal;
+        }
+
+        boolean isPentagonal(long value) {
+            while (value > this.list.last()) {
+                long pentagonal = compute(this.list.size());
+                this.list.add(pentagonal);
+            }
+
+            return this.list.containsByBinarySearch(value);
         }
     }
 }
