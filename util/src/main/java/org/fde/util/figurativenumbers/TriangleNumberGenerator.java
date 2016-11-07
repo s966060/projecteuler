7bb3c5ee166bf60161f07e59bc07a6e563ec1d26
Filip Delbaen <filipdelbaen@msn.com>
2016-11-08 00:02:14 +0100

    triangle number generator

---
+++ util/src/main/java/org/fde/util/figurativenumbers/TriangleNumberGenerator.java
@@ -0,0 +1,13 @@
+package org.fde.util.figurativenumbers;
+
+public class TriangleNumberGenerator extends FigurativeNumberGenerator {
+    public TriangleNumberGenerator() {
+        super();
+    }
+
+    @Override
+    protected long compute(long index) {
+        long triangle = index * (index + 1 / 2;
+        return triangle;
+    }
+}
