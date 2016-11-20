a6cbfa0a95fd411b187ae53c30aaca6a445ba1b2
Filip Delbaen <filipdelbaen@msn.com>
2016-11-20 19:04:13 +0100

    a test setup ... not working yet

--- solutions/src/test/java/org/fde/projecteuler/problem_049/PrimePermutations.java
+++ solutions/src/test/java/org/fde/projecteuler/problem_049/PrimePermutations.java
@@ -11,12 +11,42 @@ import java.util.Collections;
 import java.util.List;
 
 import static java.lang.System.out;
+import static org.junit.Assert.assertTrue;
 
 public class PrimePermutations {
     @Test
     public void primePermutations() {
         // List<Target> suspects = getAllSuspects();
-        List<Target> suspects = Arrays.asList(new Target(1487, 1), new Target(4817, 1), new Target(8147, 1));
+        List<Target> suspects = Arrays.asList(
+                new Target(1478, 1), // 1
+                new Target(1487, 1), // 2
+                new Target(1748, 1), // 3
+                new Target(1784, 1), // 4
+                new Target(1847, 1), // 5
+                new Target(1874, 1), // 6
+
+                new Target(4178, 1), // 7
+                new Target(4187, 1), // 8
+                new Target(4718, 1), // 9
+                new Target(4781, 1), // 10
+                new Target(4817, 1), // 11
+                new Target(4871, 1), // 12
+
+                new Target(7148, 1), // 13
+                new Target(7184, 1), // 14
+                new Target(7418, 1), // 15
+                new Target(7481, 1), // 16
+                new Target(7814, 1), // 17
+                new Target(7841, 1), // 18
+
+                new Target(8147, 1), // 19
+                new Target(8174, 1), // 20
+                new Target(8417, 1), // 21
+                new Target(8471, 1), // 22
+                new Target(8714, 1), // 23
+                new Target(8741, 1)  // 24
+        );
+
         // out.println("suspects = " + suspects);
 
         TargetMap sameCanonicals = getWithSameCanonical(suspects);
@@ -30,22 +60,25 @@ public class PrimePermutations {
 
             differences.forEach((difference, suspectTargets) -> {
                 if (suspectTargets.size() > 2) {
+                    out.println("difference = " + difference);
                     out.println(suspectTargets);
                 }
             });
         });
+
+        assertTrue(false); // "not solved yet"
     }
 
     private TargetMap retainAtLeastWithThreeTargets(TargetMap sameCanonicals) {
-        TargetMap onlyThree = new TargetMap();
+        TargetMap atLeastThree = new TargetMap();
 
         sameCanonicals.forEach((k, v) -> {
-            if (v.size() == 3) {
-                onlyThree.put(k, v);
+            if (v.size() >= 3) {
+                atLeastThree.put(k, v);
             }
         });
 
-        return onlyThree;
+        return atLeastThree;
     }
 
     private TargetMap getWithSameCanonical(List<Target> targets) {
