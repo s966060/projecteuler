bce22dddd65744a6b910349c106491b1d0c1319a
Filip Delbaen <filipdelbaen@msn.com>
2016-11-27 23:29:26 +0100

    time spent in valueOf

--- util/src/main/java/org/fde/util/Digit.java
+++ util/src/main/java/org/fde/util/Digit.java
@@ -1,6 +1,7 @@
 package org.fde.util;
 
 import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.time.DurationFormatUtils;
 
 public class Digit implements Comparable<Digit> {
     private final long digit;
@@ -9,9 +10,26 @@ public class Digit implements Comparable<Digit> {
         this.digit = digit;
     }
 
+    private static long TOTAL_ELAPSED =0 ;
+    private static final long LOG_THRESHOLD = 1_000_000_000;
+    private static long LOG_ELAPSED = LOG_THRESHOLD;
+
     public static Digit valueOf(long digit) {
-        Validate.inclusiveBetween(0, 9, digit);
-        return new Digit(digit);
+        long start = System.nanoTime();
+
+        try {
+            Validate.inclusiveBetween(0, 9, digit);
+            return new Digit(digit);
+        }
+        finally {
+            long elapsed = System.nanoTime() - start;
+            TOTAL_ELAPSED += elapsed;
+
+            if(TOTAL_ELAPSED > LOG_ELAPSED) {
+                LOG_ELAPSED += LOG_THRESHOLD;
+                System.out.println("@@@ Digit.valueOf() ... elapsed = " + DurationFormatUtils.formatDurationHMS(TOTAL_ELAPSED / 1_000_000));
+            }
+        }
     }
 
     @Override
