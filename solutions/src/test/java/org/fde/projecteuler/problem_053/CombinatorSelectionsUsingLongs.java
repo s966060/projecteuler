6fc744928b12617053657f921da4d9539ebb5c49
Filip Delbaen <filipdelbaen@msn.com>
2016-12-03 01:06:05 +0100

    a second solution

---
+++ solutions/src/test/java/org/fde/projecteuler/problem_053/CombinatorSelectionsUsingLongs.java
@@ -0,0 +1,80 @@
+package org.fde.projecteuler.problem_053;
+
+import org.fde.util.ListOfLong;
+import org.fde.util.LongUtil;
+import org.fde.util.primes.PrimeFactors;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CombinatorSelectionsUsingLongs {
+    @Test
+    public void combinatorSelections() {
+        int count = 0;
+
+        for (int n = 1; n <= 100; ++n) {
+            int midpoint = n / 2;
+            boolean overAMillion = false;
+
+            int r = 0;
+
+            while (r <= midpoint && !overAMillion) {
+                long result = combination(n, r);
+                // System.out.printf("result = C(%s, %s) = %s%n", n, r, result);
+
+                if (result > 1_000_000) {
+                    overAMillion = true;
+                } else {
+                    ++r;
+                }
+            }
+
+            if (overAMillion) {
+                count += (midpoint - r) * 2; // all except the mid points
+                count += LongUtil.isEven(n) ? 1 : 2;
+                // System.out.println("count = " + count);
+            }
+        }
+
+        System.out.println("!!! count = " + count);
+        assertEquals(4075, count);
+    }
+
+    private long combination(int n, int r) {
+        ListOfLong nFactors = factorial(n);
+        ListOfLong rFactors = factorial(r);
+        ListOfLong nMinusRFactors = factorial(n - r);
+
+        nFactors.remove(rFactors);
+        nFactors.remove(nMinusRFactors);
+
+        long result = 1;
+
+        for (long factor : nFactors) {
+            result *= factor;
+
+            if (result > 1_000_000) {
+                return Long.MAX_VALUE;
+            }
+        }
+
+        return result;
+    }
+
+    private final PrimeFactors primeFactors = new PrimeFactors();
+
+    private ListOfLong factorial(int n) {
+        if (n == 0) {
+            return new ListOfLong();
+        }
+
+        ListOfLong allFactors = new ListOfLong();
+
+        for (int i = 1; i <= n; ++i) {
+            ListOfLong factors = primeFactors.getPrimeFactors(i);
+            allFactors.addAll(factors);
+        }
+
+        return allFactors;
+    }
+}
