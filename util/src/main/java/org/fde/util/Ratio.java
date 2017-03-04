28f0a75ff6b4349e8b3883737d62740d0979ce9c
Filip Delbaen <filipdelbaen@msn.com>
2017-03-04 21:25:14 +0100

    warn on slow prime factorization

--- util/src/main/java/org/fde/util/Ratio.java
+++ util/src/main/java/org/fde/util/Ratio.java
@@ -1,5 +1,6 @@
 package org.fde.util;
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.fde.util.primes.PrimeFactors;
 
 public class Ratio {
@@ -48,10 +49,25 @@ public class Ratio {
     }
 
     private ListOfLong getCommonFactors(PrimeFactors factors) {
-        ListOfLong nominatorFactors = factors.getPrimeFactors(this.nominator);
-        ListOfLong denominatorFactors = factors.getPrimeFactors(this.denominator);
+        ListOfLong nominatorFactors = getPrimeFactors(factors, this.nominator);
+        ListOfLong denominatorFactors = getPrimeFactors(factors, this.denominator);
 
         return nominatorFactors.getIntersection(denominatorFactors);
+    }
+
+    private ListOfLong getPrimeFactors(PrimeFactors factors, long value) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        try {
+            return factors.getPrimeFactors(value);
+        }
+        finally {
+            if(stopWatch.getTime() > 1_000) {
+                System.out.printf("(%,d) took a long time (%s) to factorize %n",
+                        value, stopWatch);
+            }
+        }
     }
 
     @Override
