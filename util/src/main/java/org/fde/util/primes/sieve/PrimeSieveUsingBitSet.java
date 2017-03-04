584ab47ed696dc94804f90ffb2ed83054df86e1c
Filip Delbaen <filipdelbaen@msn.com>
2017-03-05 00:33:10 +0100

    the prime sive using a bit set as base data structure

---
+++ util/src/main/java/org/fde/util/primes/sieve/PrimeSieveUsingBitSet.java
@@ -0,0 +1,108 @@
+package org.fde.util.primes.sieve;
+
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Iterator;
+
+public class PrimeSieveUsingBitSet implements PrimeSieve {
+    private final int length;
+    private final BitSet numbers;
+
+    public PrimeSieveUsingBitSet(int upTo) {
+        this.length = upTo + 1;
+        this.numbers = new BitSet(length);
+    }
+
+    @Override
+    public void sieve() {
+        int limit = getLimit();
+
+        setComposite(0, true);
+        setComposite(1, true);
+        setComposite(2, false);
+
+        for (int index = 0; index < limit; ++index) {
+            if (isPrime(index)) {
+                int prime = index;
+
+                eliminateComposites(prime);
+            }
+        }
+    }
+
+    private void eliminateComposites(int prime) {
+        int composite;
+        composite = prime + prime;
+
+        while (composite < getLength()) {
+            setComposite(composite, true);
+            composite += prime;
+        }
+    }
+
+    private int getLimit() {
+        int primeLimit = (int) Math.sqrt(getLength()) + 1;
+        return Math.min(primeLimit, getLength());
+    }
+
+    @Override
+    public Iterable<Long> getPrimes() {
+        return getPrimeIterable();
+    }
+
+    private Iterable<Long> getPrimeIterable() {
+        return () -> getPrimeIterator();
+    }
+
+    private Iterator<Long> getPrimeIterator() {
+        return new Iterator<Long>() {
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                for (int search = index; search < getLength(); ++search) {
+                    if (isPrime(search)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            @Override
+            public Long next() {
+                for (int search = index; search < getLength(); ++search) {
+                    if (isPrime(search)) {
+                        index = search + 1;
+                        return (long) search;
+                    }
+                }
+
+                throw new IllegalArgumentException("no next found");
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        return "PrimeSieve{" +
+                "numbers=" + numbers +
+                '}';
+    }
+
+    boolean[] getNumbers() {
+        return null;
+    }
+
+    private boolean isPrime(int index) {
+        return !this.numbers.get(index);
+    }
+
+    private int getLength() {
+        return length;
+    }
+
+    private void setComposite(int index, boolean isComposite) {
+        this.numbers.set(index,isComposite);
+    }
+}
---
+++ util/src/test/java/org/fde/util/primes/sieve/PrimeSieveUsingBitSetTest.java
@@ -0,0 +1,30 @@
+package org.fde.util.primes.sieve;
+
+import org.fde.util.primes.PrimeBuilder;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrimeSieveUsingBitSetTest {
+    @Test
+    public void primeSieveUsingBitSetTest() {
+        PrimeSieve sieve = createPrimeSieve(10_000_000);
+        sieve.sieve();
+
+        PrimeBuilder builder = new PrimeBuilder();
+
+        int count = 0;
+
+        for (Long prime : sieve.getPrimes()) {
+            long expected = builder.next();
+            assertEquals(expected, prime.longValue());
+            ++count;
+        }
+
+        assertEquals(664579, count);
+    }
+
+    private PrimeSieve createPrimeSieve(int upTo) {
+        return new PrimeSieveUsingBitSet(10_000_000);
+    }
+}
