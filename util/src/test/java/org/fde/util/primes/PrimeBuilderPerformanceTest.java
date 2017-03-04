5e23e7b293269dc38f9730c104cf183cc7f70ab8
Filip Delbaen <filipdelbaen@msn.com>
2017-03-04 21:25:35 +0100

    up to 100 million

--- util/src/test/java/org/fde/util/primes/PrimeBuilderPerformanceTest.java
+++ util/src/test/java/org/fde/util/primes/PrimeBuilderPerformanceTest.java
@@ -15,10 +15,10 @@ public class PrimeBuilderPerformanceTest {
 
         while(builder.next() < 1_000_000);
 
-        assertEquals(1_000_003L, builder.current());
-
         // about 200 milliseconds on my iCore 7 3770
         System.out.println("stopWatch = " + stopWatch);
+
+        assertEquals(1_000_003L, builder.current());
     }
 
     @Test
@@ -30,9 +30,24 @@ public class PrimeBuilderPerformanceTest {
 
         while(builder.next() < 10_000_000);
 
-        assertEquals(10_000_019L, builder.current());
-
         // about 3.5 seconds on my iCore 7 3770
         System.out.println("stopWatch = " + stopWatch);
+
+        assertEquals(10_000_019L, builder.current());
+    }
+
+    @Test
+    public void upToHundredMillion() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        PrimeBuilder builder = new PrimeBuilder();
+
+        while(builder.next() < 100_000_000);
+
+        // about 1.5 minute on my iCore 7 3770
+        System.out.println("stopWatch = " + stopWatch);
+
+        assertEquals(100_000_007L, builder.current());
     }
 }
