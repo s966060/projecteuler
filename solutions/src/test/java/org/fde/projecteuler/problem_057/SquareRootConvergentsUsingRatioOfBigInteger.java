c911473f6a41a776c90859ad91c8595e2cb87ec0
Filip Delbaen <filipdelbaen@msn.com>
2017-03-05 23:37:14 +0100

    clean up

--- solutions/src/test/java/org/fde/projecteuler/problem_057/SquareRootConvergentsUsingRatioOfBigInteger.java
+++ solutions/src/test/java/org/fde/projecteuler/problem_057/SquareRootConvergentsUsingRatioOfBigInteger.java
@@ -4,7 +4,6 @@ import org.fde.util.RatioOfBigInteger;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 public class SquareRootConvergentsUsingRatioOfBigInteger {
     @Test
