36f366512794b3276e95590d595d3042589c9646
Filip Delbaen <filipdelbaen@msn.com>
2017-04-01 09:28:13 +0200

    cubic permutations

--- solutions/src/test/java/org/fde/projecteuler/problem_062/CubicPermutations.java
+++ solutions/src/test/java/org/fde/projecteuler/problem_062/CubicPermutations.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CubicPermutations {
-
     @Test
     public void cubicPermutations() {
         MyMap map = new MyMap();
