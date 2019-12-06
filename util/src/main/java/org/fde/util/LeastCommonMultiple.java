ee933c0f2b59618375fedca665fa001c543360db
filip delbaen <filipdelbaen@msn.com>
2019-12-06 18:25:47 +0100

    lcm big integer

--- util/src/main/java/org/fde/util/LeastCommonMultiple.java
+++ util/src/main/java/org/fde/util/LeastCommonMultiple.java
@@ -1,11 +1,19 @@
 package org.fde.util;
 
+import java.math.BigInteger;
+
 import static org.fde.util.GreatestCommonDivisor.gcd;
 
 public class LeastCommonMultiple {
     public static long lcm(long a, long b) {
         long gcd = gcd(a, b);
         long c = a * b / gcd;
+        return c;
+    }
+
+    public static BigInteger lcm(BigInteger a, BigInteger b) {
+        BigInteger gcd = a.gcd(b);
+        BigInteger c = a.multiply(b).divide(gcd);
         return c;
     }
 }
