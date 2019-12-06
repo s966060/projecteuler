2c276c13e6029d43287adb4529ce71f8dfb8bb1d
filip delbaen <filipdelbaen@msn.com>
2019-12-06 18:26:00 +0100

    problem 65 : with big integer

--- solutions/src/test/java/org/fde/projecteuler/problem_065/ConvergentsOfEProblem.java
+++ solutions/src/test/java/org/fde/projecteuler/problem_065/ConvergentsOfEProblem.java
@@ -2,36 +2,38 @@ package org.fde.projecteuler.problem_065;
 
 import org.junit.Test;
 
-import static org.fde.projecteuler.problem_065.ConvergentsOfEProblem.Number.number;
+import java.math.BigInteger;
+
+import static org.fde.projecteuler.problem_065.ConvergentsOfEProblem.NumberLong.numberLong;
 import static org.fde.util.LeastCommonMultiple.lcm;
 
 public class ConvergentsOfEProblem {
-    static class Number {
+    static class NumberLong {
         final long n;
         final long d;
 
-        public Number(long numerator, long d) {
+        public NumberLong(long numerator, long d) {
             this.n = numerator;
             this.d = d;
         }
 
-        public Number(long n) {
+        public NumberLong(long n) {
             this(n, 1);
         }
 
-        static Number number(long n, long d) {
-            return new Number(n, d);
+        static NumberLong numberLong(long n, long d) {
+            return new NumberLong(n, d);
         }
 
-        static Number number(long n) {
-            return new Number(n, 1);
+        static NumberLong numberLong(long n) {
+            return new NumberLong(n, 1);
         }
 
-        Number plus(Number other) {
+        NumberLong plus(NumberLong other) {
             long lcm = lcm(this.d, other.d);
 
             long x = (this.n * (lcm / this.d)) + (other.n * (lcm / other.d));
-            return number(x, lcm);
+            return numberLong(x, lcm);
         }
 
         @Override
@@ -42,22 +44,66 @@ public class ConvergentsOfEProblem {
                     '}';
         }
 
-        static Number ZERO = number(0);
-        static Number ONE = number(1);
+        static NumberLong ZERO = numberLong(0);
+        static NumberLong ONE = numberLong(1);
 
-        public Number inverse() {
-            return number(this.d, this.n);
+        public NumberLong inverse() {
+            return numberLong(this.d, this.n);
+        }
+    }
+
+    static class NumberBigInteger {
+        final BigInteger n;
+        final BigInteger d;
+
+        public NumberBigInteger(BigInteger numerator, BigInteger d) {
+            this.n = numerator;
+            this.d = d;
+        }
+
+        public NumberBigInteger(BigInteger n) {
+            this(n, BigInteger.ONE);
+        }
+
+        static NumberBigInteger numberBigInteger(BigInteger n, BigInteger d) {
+            return new NumberBigInteger(n, d);
+        }
+
+        static NumberBigInteger numberBigInteger(BigInteger n) {
+            return new NumberBigInteger(n, BigInteger.ONE);
+        }
+
+        NumberBigInteger plus(NumberBigInteger other) {
+            BigInteger lcm = lcm(this.d, other.d);
+
+            BigInteger x = (this.n.multiply(lcm).divide(this.d)).add((other.n.multiply(lcm).divide(other.d)));
+            return numberBigInteger(x, lcm);
+        }
+
+        @Override
+        public String toString() {
+            return "Number{" +
+                    "numerator=" + n +
+                    ", denomitator=" + d +
+                    '}';
+        }
+
+        static NumberBigInteger ZERO = numberBigInteger(BigInteger.ZERO);
+        static NumberBigInteger ONE = numberBigInteger(BigInteger.ONE);
+
+        public NumberBigInteger inverse() {
+            return numberBigInteger(this.d, this.n);
         }
     }
 
     @Test
-    public void f () {
-        Number result = Number.ZERO;
+    public void testWithLong () {
+        NumberLong result = NumberLong.ZERO;
 
         for (int k = 99; k >= 0; --k) {
-            long factor = factor(k);
+            long factor = factorLong(k);
 
-            result = number(factor).plus(result);
+            result = NumberLong.numberLong(factor).plus(result);
 
             if (k > 0) {
                 result = result.inverse();
@@ -67,7 +113,24 @@ public class ConvergentsOfEProblem {
         System.out.println("result = " + result);
     }
 
-    private long factor(int j) {
+    @Test
+    public void testWithBigInteger () {
+        NumberBigInteger result = NumberBigInteger.ZERO;
+
+        for (int k = 99; k >= 0; --k) {
+            BigInteger factor = BigInteger.valueOf(factorLong(k));
+
+            result = NumberBigInteger.numberBigInteger(factor).plus(result);
+
+            if (k > 0) {
+                result = result.inverse();
+            }
+        }
+
+        System.out.println("result = " + result);
+    }
+
+    private long factorLong(int j) {
         if (j == 0) {
             return 2;
         }
